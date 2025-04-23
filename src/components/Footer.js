b31d4e326fe6fce2777e3963424316ab7ced8666
Jose Pascuas <josemi0@hotmail.com>
2025-04-22 22:06:33 -0500

    se completa el componente footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,35 @@ const Footer= ()=> {
         <img src={restaurante} alt="imagen del balcon" width={150} height={210}/>
         <img src={logo} alt="logo"/>
       </div>
+      <div>
+        <div>
+          <h1>Navegar</h1>
+          <ul>
+            <li>Inicio</li>
+            <li>Nosotros</li>
+            <li>Menú</li>
+            <li>Reservas</li>
+            <li>Ordenar</li>
+            <li>Iniciar sesion</li>
+          </ul>
+        </div>
+        <div>
+          <h1>Contacto</h1>
+          <ul>
+            <li>Dirección</li>
+            <li>Número</li>
+            <li>Email</li>
+          </ul>
+        </div>
+        <div>
+          <h1>Redes Sociales</h1>
+          <ul>
+            <li>Instagram</li>
+            <li>Facebook</li>
+            <li>Whatsapp</li>
+          </ul>
+        </div>
+      </div>
     </section>
   )
 }
