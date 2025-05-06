3e1d3d26a7c46b1b338a11ac75495680f48ff823
Jose Pascuas <josemi0@hotmail.com>
2025-05-06 00:01:23 -0500

    estilizacion componente footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,38 +3,39 @@ import restaurante from "../assets/images/restaurante.jpg"
 
 const Footer= ()=> {
   return(
-    <section>
-      <div>
-        <img src={restaurante} alt="imagen del balcon" width={150} height={210}/>
-        <img src={logo} alt="logo"/>
-      </div>
-      <div>
-        <div>
-          <h1>Navegar</h1>
-          <ul>
-            <li>Inicio</li>
-            <li>Nosotros</li>
-            <li>Menú</li>
-            <li>Reservas</li>
-            <li>Ordenar</li>
-            <li>Iniciar sesion</li>
-          </ul>
+    <section className="flex flex-col md:flex-row mt-20 w-full justify-center mb-20">
+      <div className="flex flex-col md:flex-row md:w-[880px] justify-between">
+        <div className="flex flex-col justify-center items-center mb-10 md:mb-0 md:mr-5">
+          <img className="w-[203px] h-[198px] rounded-xl mb-5" src={restaurante} alt="imagen del balcon"/>
+          <img className="w-[176px] h-[45px]" src={logo} alt="logo"/>
         </div>
-        <div>
-          <h1>Contacto</h1>
-          <ul>
-            <li>Dirección</li>
-            <li>Número</li>
-            <li>Email</li>
-          </ul>
-        </div>
-        <div>
-          <h1>Redes Sociales</h1>
-          <ul>
-            <li>Instagram</li>
-            <li>Facebook</li>
-            <li>Whatsapp</li>
-          </ul>
+        <div className="flex flex-col md:flex-row gap-5 md:gap-[80px] ml-10">
+          <div>
+            <h1 className="font-markazi text-[40px] font-normal">Navegar</h1>
+            <ul className="font-karla text-lg font-bold">
+              <li><a href="#">Inicio</a></li>
+              <li><a href="#">Nosotros</a></li>
+              <li><a href="#">Menú</a></li>
+              <li><a href="#">Reservas</a></li>
+              <li><a href="#">Ordenar</a></li>
+            </ul>
+          </div>
+          <div>
+            <h1 className="font-markazi text-[40px] font-normal">Contacto</h1>
+            <ul className="font-karla text-lg font-bold">
+              <li><a href="#">Dirección</a></li>
+              <li><a href="#">Número</a></li>
+              <li><a href="#">Email</a></li>
+            </ul>
+          </div>
+          <div>
+            <h1 className="font-markazi text-[40px] font-normal">Redes Sociales</h1>
+            <ul className="font-karla text-lg font-bold">
+              <li><a href="#">Instagram</a></li>
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Whatsapp</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
