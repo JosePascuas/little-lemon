ebe03551a7d9b5a26f3362ff6678d81e883e44df
Jose Pascuas <josemi0@hotmail.com>
2025-05-25 19:44:09 -0500

    cambios

--- src/components/Reservasform.js
+++ src/components/Reservasform.js
@@ -113,7 +113,7 @@ const Reservasform = () => {
     }
 
     try {
-      const response = await fetch("http://little-lemon-backend-production.up.railway.app/reservas", {
+      const response = await fetch("https://little-lemon-backend-production.up.railway.app/reservas", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
