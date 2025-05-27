68ad280cd7800d031400172ebcf4793551e982c1
Jose Pascuas <josemi0@hotmail.com>
2025-05-26 21:15:07 -0500

    fix

--- src/components/Reservasform.js
+++ src/components/Reservasform.js
@@ -80,7 +80,7 @@ const Reservasform = () => {
     tomorrow.setDate(tomorrow.getDate() + 1);
     tomorrow.setHours(0, 0, 0, 0);
 
-    if (selectedDate() < tomorrow()) {
+    if (selectedDate.getTime() < tomorrow.getTime()) {
       newErrors.fecha = "Solo puedes reservar para mañana o fechas posteriores.";
     }
   }
